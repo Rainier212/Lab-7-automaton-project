4b7f79468f6818a6a336452ba17e2a23feae9a55
Rainier <rainiertabing21@gmail.com>
2025-10-26 20:51:55 -0400

    Rewrote the update method of the Automation class

--- Automaton.java
+++ Automaton.java
@@ -48,16 +48,15 @@ public class Automaton
     {
         // Build the new state in a separate array.
         int[] nextState = new int[state.length];
+        int left = 0;
+        int center = state[0];
         // Naively update the state of each cell
         // based on the state of its two neighbors.
         for(int i = 0; i < state.length; i++) {
-            int left = (i == 0) ? 0 : state[i - 1];
-            
-            int center = state[i];
-            
-            int right = (i + 1 < state.length) ? state[i + 1] : 0;
-            
+            int right = i + 1 <state.length? state[i+1] : 0;
             nextState[i] = (left + center + right) % 2;
+            left = center;
+            center = right;
         }
         state = nextState;
     }
